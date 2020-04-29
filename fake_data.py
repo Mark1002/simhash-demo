e759ab9a92e8da2d135c913d7e4178cb80effa31
Mark Chang <ak0922747401@gmail.com>
2020-04-29 17:18:29 +0800

    update test case

--- fake_data.py
+++ fake_data.py
@@ -9,11 +9,17 @@ import secrets
 from typing import List
 
 
-def load_documents() -> List[dict]:
+def load_documents(
+    word_lower_size: int, word_upper_size: int
+) -> List[dict]:
     """Load documents from json file."""
-    with open('docs.json', 'r') as file:
+    with open('data/docs.json', 'r') as file:
         docs = json.loads(file.read())
-    return docs
+    results = []
+    for doc in docs:
+        if word_lower_size < len(str(doc['content'])) < word_upper_size:
+            results.append(doc)
+    return results
 
 
 def gen_datetime(min_year=1900, max_year=datetime.datetime.now().year):
@@ -30,16 +36,17 @@ def gen_random_fid() -> str:
     return fids[random.randint(0, len(fids)-1)]
 
 
-def fake_doc_generator(num: int, same_doc: bool = False) -> iter:
+def fake_doc_generator(
+    docs: list, num: int,
+    same_doc: bool = False,
+) -> iter:
     """Fake gen."""
-    docs = load_documents()
     doc = docs[random.randint(0, len(docs)-1)]
     for _ in range(num):
         if same_doc:
             doc = copy.copy(doc)
         else:
             doc = docs[random.randint(0, len(docs)-1)]
-        doc['uid'] = gen_random_fid()
         doc['created_time'] = gen_datetime(min_year=2019).strftime("%Y-%m-%d %H:%M:%S") # noqa
         doc['md5_id'] = secrets.token_hex(nbytes=16)
         yield doc
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@
 import secrets
 from redis import StrictRedis
 from log_service import logging
-from fake_data import fake_doc_generator
+from fake_data import fake_doc_generator, load_documents
 from sim_hash import perform_simhash_filter
 
 
@@ -15,7 +15,7 @@ def setup_function(function):
 def test_same_hash_but_differ_netloc():
     """Test the same hash but differ netloc."""
     dup_num = 0
-    doc = list(fake_doc_generator(1))[0]
+    doc = list(fake_doc_generator(docs=load_documents(0, 9999), num=1))[0]
     dup_num += perform_simhash_filter(doc)
     # make up differ netloc
     doc['link'] = 'https://www.yahoo.com/post123'
@@ -24,13 +24,29 @@ def test_same_hash_but_differ_netloc():
     assert dup_num == 0
 
 
-def test_duplicate_docs():
-    """Test fake duplicate docs."""
-    dup_num = 0
-    docs = fake_doc_generator(num=100)
+def _test_duplicate_docs(lower: int, upper: int):
+    """Test fake duplicate docs logic."""
+    # add unique docs
+    docs = load_documents(lower, upper)
     for doc in docs:
-        dup_num += perform_simhash_filter(doc, hmm_dis=3)
-    logging.debug(f'near duplicate number: {dup_num}')
+        perform_simhash_filter(doc, hmm_dis=3)
+    # add near duplicate docs
+    dup_counter = 0
+    dup_num = 1000
+    docs = fake_doc_generator(docs=docs, num=dup_num)
+    for doc in docs:
+        dup_counter += perform_simhash_filter(doc, hmm_dis=3)
+    logging.debug(f'near duplicate number: {dup_counter}, hit rate: {dup_counter / dup_num}') # noqa
+
+
+def test_duplicate_long_docs():
+    """Test for long duplicate docs."""
+    _test_duplicate_docs(600, 9999)
+
+
+def test_duplicate_short_docs():
+    """Test for short duplicate docs."""
+    _test_duplicate_docs(0, 100)
 
 
 def teardown_function(function):
