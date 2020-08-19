8f99be6e2865164a35179e0f77797afd9c3c2418
Mark Chang <ak0922747401@gmail.com>
2020-08-19 11:27:18 +0800

    fix arg and import

--- sim_hash.py
+++ sim_hash.py
@@ -6,7 +6,7 @@ from log_service import logging
 from redis import StrictRedis
 from simhash import Simhash
 
-from fake_data import fake_doc_generator
+from fake_data import load_documents, fake_doc_generator
 from urllib.parse import urlparse
 
 r_md5_id_client = StrictRedis(db=2)
@@ -76,8 +76,8 @@ def perform_simhash_filter(doc: dict, hmm_dis: int = 3) -> int:
 
 if __name__ == '__main__':
     dup_num = 0
-    # docs = load_documents()
-    docs = fake_doc_generator(num=100)
+    docs = load_documents(400, 500)
+    docs = fake_doc_generator(docs=docs, num=100)
     for doc in docs:
         dup_num += perform_simhash_filter(doc)
-    print(f'near duplicate number: {dup_num}')
+    logging.debug(f'near duplicate number: {dup_num}')
